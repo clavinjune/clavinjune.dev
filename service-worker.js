42f47dad829e1dcbe3781fe69d4d0b81c066be97
Clavin June <juneardoc@gmail.com>
2019-10-27 18:33:09 +0700

    rebuild

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45f0b0112972cfb1acc904635a53874d.js"
+  "/precache-manifest.70661aa9f68671abe3aa2735734665d5.js"
 );
 
 self.addEventListener('message', (event) => {
